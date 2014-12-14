c9f0e508d398d927de17861df00d86207d3aa600
Tomoatsu Shimada <shimada@walbrix.com>
2014-12-14 18:11:52 +0900

    prefix was wrong

--- default_config.py
+++ default_config.py
@@ -1,4 +1,4 @@
-CMS_BASE="http://localhost:8080/cms/public/wbrxcorp"
+CMS_BASE="http://localhost:8080/cms/public/walbrix"
 GMAIL_ID="your_gmail_id@gmail.com"
 GMAIL_PASSWORD="your_gmail_password"
 # put local_config.py to override above
